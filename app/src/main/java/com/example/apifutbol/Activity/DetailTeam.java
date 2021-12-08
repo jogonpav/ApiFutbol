0ae712bc2185de132883b3ef4bbb7a6c5d7042cc
jogonpav <josepabonvera@gmail.com>
2021-12-08 13:05:42 -0500

    Se realiza activity para ver información de un equipo

---
+++ app/src/main/java/com/example/apifutbol/Activity/DetailTeam.java
@@ -0,0 +1,14 @@
+package com.example.apifutbol.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailTeam extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_team);
+    }
+}
